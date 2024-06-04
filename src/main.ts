369152dce1ee99e9b6e01d07ff11f2058d8f1116
Antonio880 <antoniocruzgomes880@gmail.com>
2024-06-04 15:21:36 -0300

    docs: criação da documentação com swagger de forma inicial

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: false });
+  const config = new DocumentBuilder()
+    .setTitle('Documentation of Ifood API')
+    .setDescription('The documentation of the Ifood API.')
+    .setVersion('1.0')
+    .addTag('Ifood')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
